ae76a45463943c613e15aa601ae2f39aad5b9546
mason.kohls <mason.kohls1999@gmail.com>
2016-09-14 14:35:04 -0700

    ndhz

---
+++ GeneticAlg/src/components/Breeder.java
@@ -0,0 +1,21 @@
+package components;
+
+import java.util.List;
+
+import components.mathsolver.Tree;
+
+public class Breeder {
+	
+	private MutationHelper helper;
+	public Breeder(){
+		
+	}
+	
+	public List<Tree> simpleBreed(){
+		
+		
+		
+		return null;
+		
+	}
+}
