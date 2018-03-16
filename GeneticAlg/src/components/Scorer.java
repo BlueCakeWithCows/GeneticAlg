37f77246fe13e9cf178c70ed01445de71fdeb8d2
=bluecakewithcows <mason.kohls1999@gmail.com>
2018-03-16 16:39:38 -0700

    Fixd some bugs

--- GeneticAlg/src/components/Scorer.java
+++ GeneticAlg/src/components/Scorer.java
@@ -1,181 +1,188 @@
 package components;
 
+import components.mathsolver.Tree;
+
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.List;
 import java.util.Random;
 
-import components.mathsolver.Tree;
-
 public abstract class Scorer {
 
-	private List<TestCase> totalSetOfTests;
-	private double percentOfTestsToUseEachRun;
-	private boolean cheatRandomOnDataPercent;
+    private List<TestCase> totalSetOfTests;
+    private double percentOfTestsToUseEachRun;
+    private boolean cheatRandomOnDataPercent;
 
-	private Random random;
+    private Random random;
 
-	public Scorer(List<TestCase> cases, double testDataToUse, Random r) {
-		this.totalSetOfTests = cases;
-		percentOfTestsToUseEachRun = testDataToUse;
-		this.random = r;
-		this.cheatRandomOnDataPercent = false;
-	}
+    public Scorer(List<TestCase> cases, double testDataToUse, Random r) {
+        this.totalSetOfTests = cases;
+        percentOfTestsToUseEachRun = testDataToUse;
+        this.random = r;
+        this.cheatRandomOnDataPercent = false;
+    }
 
-	public Scorer() {
+    public Scorer() {
 
-	}
+    }
 
-	private List<TestCase> getTestingSet() {
-		// If its one don't bother
-		if (percentOfTestsToUseEachRun == 1)
-			return totalSetOfTests;
-		List<TestCase> cases;
+    private List<TestCase> getTestingSet() {
+        // If its one don't bother
+        if (percentOfTestsToUseEachRun == 1) {
+            return totalSetOfTests;
+        }
+        List<TestCase> cases;
 
-		// Cheat by using range rather than random ones
-		if (cheatRandomOnDataPercent) {
-			double start = random.nextDouble();
-			double end = (start + percentOfTestsToUseEachRun);
-			if (end < 1) {
-				return totalSetOfTests.subList((int) (start * totalSetOfTests.size()),
-						(int) (end * totalSetOfTests.size()));
-			} else {
-				int index1 = (int) (start * totalSetOfTests.size());
-				int index2 = (int) ((end - 1) * totalSetOfTests.size());
+        // Cheat by using range rather than random ones
+        if (cheatRandomOnDataPercent) {
+            double start = random.nextDouble();
+            double end = (start + percentOfTestsToUseEachRun);
+            if (end < 1) {
+                return totalSetOfTests.subList((int) (start * totalSetOfTests.size()),
+                    (int) (end * totalSetOfTests.size()));
+            } else {
+                int index1 = (int) (start * totalSetOfTests.size());
+                int index2 = (int) ((end - 1) * totalSetOfTests.size());
 
-				cases = totalSetOfTests.subList(0, index2);
-				cases.addAll(totalSetOfTests.subList(index1, totalSetOfTests.size()));
-				return cases;
-			}
-		}
+                cases = totalSetOfTests.subList(0, index2);
+                cases.addAll(totalSetOfTests.subList(index1, totalSetOfTests.size()));
+                return cases;
+            }
+        }
 
-		// Pick randomly, repeats allowed
-		cases = new ArrayList<TestCase>();
-		for (int i = 0; i < totalSetOfTests.size() * percentOfTestsToUseEachRun; i++) {
-			cases.add(totalSetOfTests.get(random.nextInt(totalSetOfTests.size())));
-		}
-		return cases;
+        // Pick randomly, repeats allowed
+        cases = new ArrayList<TestCase>();
+        for (int i = 0; i < totalSetOfTests.size() * percentOfTestsToUseEachRun; i++) {
+            cases.add(totalSetOfTests.get(random.nextInt(totalSetOfTests.size())));
+        }
+        return cases;
 
-	}
+    }
 
-	public void setRandom(Random random) {
-		this.random = random;
-	}
+    public void setRandom(Random random) {
+        this.random = random;
+    }
 
-	public void setTestSuite(List<TestCase> cases) {
-		this.totalSetOfTests = cases;
-	}
+    public void setTestSuite(List<TestCase> cases) {
+        this.totalSetOfTests = cases;
+    }
 
-	public void setUsableTestSuitePercent(double percent) {
-		this.percentOfTestsToUseEachRun = percent;
-	}
+    public void setUsableTestSuitePercent(double percent) {
+        this.percentOfTestsToUseEachRun = percent;
+    }
 
-	public void setQuickRandom(boolean cheat) {
-		this.cheatRandomOnDataPercent = cheat;
-	}
+    public void setQuickRandom(boolean cheat) {
+        this.cheatRandomOnDataPercent = cheat;
+    }
 
-	/** Returns List ordered best to worst */
-	protected abstract List<ScoredTree> score(List<Tree> children, List<TestCase> cases);
+    /**
+     * Returns List ordered best to worst
+     */
+    protected abstract List<ScoredTree> score(List<Tree> children, List<TestCase> cases);
 
-	public List<Tree> parallelScore(List<Tree> children, int numberOfThreads) {
-		List<TestCase> cases = this.getTestingSet();
+    public List<Tree> parallelScore(List<Tree> children, int numberOfThreads) {
+        List<TestCase> cases = this.getTestingSet();
 
-		List<Tree> needToBeScored = new ArrayList<Tree>();
-		for (Tree tree : children) {
-			if (!tree.scored)
-				needToBeScored.add(tree);
-			tree.scored = true;
-		}
-		children.removeAll(needToBeScored);
-		ParallelScorer[] threads = new ParallelScorer[numberOfThreads];
-		int min = 0, max = 0;
-		int delta = (needToBeScored.size()) / numberOfThreads;
-		int i2 = 0;
-		for (; i2 < numberOfThreads - 1; i2++) {
-			max = min + delta;
-			List<Tree> subList = needToBeScored.subList(min, max);
-			threads[i2] = new ParallelScorer(subList, cases);
-			threads[i2].start();
-			min += delta;
-		}
-		List<Tree> subList = needToBeScored.subList(min, needToBeScored.size());
-		threads[i2] = new ParallelScorer(subList, cases);
-		threads[i2].start();
+        List<Tree> needToBeScored = new ArrayList<Tree>();
+        for (Tree tree : children) {
+            if (!tree.scored) {
+                needToBeScored.add(tree);
+            }
+            tree.scored = true;
+        }
+        children.removeAll(needToBeScored);
+        ParallelScorer[] threads = new ParallelScorer[numberOfThreads];
+        int min = 0, max = 0;
+        int delta = (needToBeScored.size()) / numberOfThreads;
+        int i2 = 0;
+        for (; i2 < numberOfThreads - 1; i2++) {
+            max = min + delta;
+            List<Tree> subList = needToBeScored.subList(min, max);
+            threads[i2] = new ParallelScorer(subList, cases);
+            threads[i2].start();
+            min += delta;
+        }
+        List<Tree> subList = needToBeScored.subList(min, needToBeScored.size());
+        threads[i2] = new ParallelScorer(subList, cases);
+        threads[i2].start();
 
-		List<ScoredTree> newList = new ArrayList<ScoredTree>();
-		for (int i = 0; i < numberOfThreads; i++) {
-			try {
+        List<ScoredTree> newList = new ArrayList<ScoredTree>();
+        for (int i = 0; i < numberOfThreads; i++) {
+            try {
 
-				threads[i].join();
-				newList.addAll(threads[i].newTrees);
-			} catch (InterruptedException e) {
-				e.printStackTrace();
-			}
-		}
-		for (Tree child : children) {
-			ScoredTree sc =new ScoredTree(child);
-			sc.runningScore = child.score;
-			newList.add(sc);
-		}
+                threads[i].join();
+                newList.addAll(threads[i].newTrees);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+        }
+        for (Tree child : children) {
+            ScoredTree sc = new ScoredTree(child);
+            sc.runningScore = child.score;
+            newList.add(sc);
+        }
 
-		sort(newList);
-		List<Tree> finalList = new ArrayList<Tree>();
+        sort(newList);
+        List<Tree> finalList = new ArrayList<Tree>();
 
-		for (ScoredTree tree : newList) {
-			finalList.add(tree.tree);
-			
-		}
-		return finalList;
-	}
+        for (ScoredTree tree : newList) {
+            finalList.add(tree.tree);
 
-	public void sort(List<ScoredTree> newList) {
-		Collections.sort(newList);
-	}
+        }
+        return finalList;
+    }
 
-	public class ParallelScorer extends Thread implements Runnable {
-		public List<Tree> trees;
-		public List<ScoredTree> newTrees;
-		public List<TestCase> cases;
+    public void sort(List<ScoredTree> newList) {
+        Collections.sort(newList);
+    }
 
-		public ParallelScorer(List<Tree> trees, List<TestCase> cases) {
-			this.trees = trees;
-			this.cases = cases;
-		}
+    public class ParallelScorer extends Thread implements Runnable {
+        public List<Tree> trees;
+        public List<ScoredTree> newTrees;
+        public List<TestCase> cases;
 
-		@Override
-		public void run() {
-			this.newTrees = score(trees, cases);
-		}
-	}
+        public ParallelScorer(List<Tree> trees, List<TestCase> cases) {
+            this.trees = trees;
+            this.cases = cases;
+        }
 
-	protected class ScoredTree implements Comparable<ScoredTree> {
-		public ScoredTree(Tree tree) {
-			this.tree = tree;
-		}
+        @Override
+        public void run() {
+            this.newTrees = score(trees, cases);
+        }
+    }
 
-		public boolean[] normaled;
+    protected class ScoredTree implements Comparable<ScoredTree> {
+        public ScoredTree(Tree tree) {
+            this.tree = tree;
+        }
 
-		Tree tree;
-		double[] tempScores;
-		double runningScore = 0;
+        public boolean[] normaled;
 
-		@Override
-		public int compareTo(ScoredTree o) {
-			int res = 0;
+        Tree tree;
+        double[] tempScores;
+        double runningScore = 0;
 
-			if (useFailedAsPrimary)
-				res = Integer.compare(this.tree.failedTests, o.tree.failedTests);
-			if (res == 0)
-				res = Double.compare(o.runningScore, this.runningScore);
-			if (res == 0)
-				res = Integer.compare(this.tree.getSize(), o.tree.getSize());
-			return res;
-		}
-	}
+        @Override
+        public int compareTo(ScoredTree o) {
+            int res = 0;
 
-	boolean useFailedAsPrimary = false;
+            if (useFailedAsPrimary) {
+                res = Integer.compare(this.tree.failedTests, o.tree.failedTests);
+            }
+            if (res == 0) {
+                res = Double.compare(o.runningScore, this.runningScore);
+            }
+            if (res == 0) {
+                //res = Integer.compare(this.tree.getSize(), o.tree.getSize());
+            }
+            return res;
+        }
+    }
 
-	public void setUseFailedAsPrimary(boolean useFailedTestsPrimaryScoring) {
-		useFailedAsPrimary = useFailedTestsPrimaryScoring;
-	}
+    boolean useFailedAsPrimary = false;
+
+    public void setUseFailedAsPrimary(boolean useFailedTestsPrimaryScoring) {
+        useFailedAsPrimary = useFailedTestsPrimaryScoring;
+    }
 }
